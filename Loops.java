4bb462507437aa80502ca43b768afbf74e41910d
sudeepbsp6 <sudeepbsp5@gmail.com>
2024-10-07 13:14:53 -0500

    just completed the loops

---
+++ Loops.java
@@ -0,0 +1,45 @@
+public class Loops 
+{
+    public static void main(String[] args)
+    {
+        //repeat this statement 4 times(loops)
+        //loop - While, do While, For. they their own use cases.
+        
+        //While Loop:
+        
+        // int i = 1;
+        // while(i<=4) 
+        // {
+        //     System.out.println("hi " + i);
+        //     int j = 1;
+        //     while (j<=3) 
+        //     {
+        //         System.out.println("Hello " + j);
+        //         j++;
+        //     }
+        //     i++;
+        // }
+        // System.out.println("Bye"+i);    
+
+        //Do While Loop:
+
+        // int i = 5;
+        // do
+        // {
+        //     System.out.println("Hi "+i);
+        //     i++;
+        // }while(i<=4);
+
+
+        //For Loop:
+
+        //Increment
+        // for(int i=1;i<=4;i++){
+        //     System.out.println("Hi "+i);
+        // }
+        //Decrement
+        // for(int i=6;i>=1;i--){
+        //     System.out.println("Hi "+i);
+        // }
+    }    
+}
